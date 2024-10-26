3b2bb8032d28fcbd1c8ee33444e5c5699718b39b
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-10-27 01:56:12 +0300

    added google analytics, and og scripts.. ( with twitter meta tags too )

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from 'next';
 import './globals.css';
 
-import { Inter } from 'next/font/google'
- 
+import { Inter } from 'next/font/google';
+import Script from 'next/script';
+
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
-})
+});
 
 export const metadata: Metadata = {
-  title: 'mikedegeofroy.com',
+  title: '@mikedegeofroy',
   description: "Mike de Geofroy's portfolio.",
   icons: {
     icon: [
@@ -25,6 +26,33 @@ export const metadata: Metadata = {
       },
     ],
   },
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://mikedegeofroy.com',
+    title: 'Mike de Geofroy',
+    description: 'Software Engineering student at ITMO University.',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 675,
+        alt: 'Software Engineering student at ITMO University.',
+      },
+    ],
+  },
+  twitter: {
+    title: 'Mike de Geofroy',
+    description: 'Software Engineering student at ITMO University.',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 675,
+        alt: 'Software Engineering student at ITMO University.',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -34,14 +62,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`mx-auto max-w-[692px] overflow-x-hidden px-6 py-12 text-gray-1200 antialiased sm:py-32 md:overflow-x-visible md:py-16 ${inter.className}`}>
-        <header className="mb-32 flex flex-col items-start">
-            <a className="text-medium inline-block font-medium no-underline" href="/">Mike de Geofroy</a>
-            <span className="text-medium font-medium leading-none text-muted">Software Engineer</span>
+      <head>
+        <Script
+          src='https://www.googletagmanager.com/gtag/js?id=G-6XF7T03ZY9'
+          strategy='afterInteractive'
+          async
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6XF7T03ZY9');
+          `}
+        </Script>
+      </head>
+      <body
+        className={`mx-auto max-w-[692px] overflow-x-hidden px-6 py-12 text-gray-1200 antialiased sm:py-32 md:overflow-x-visible md:py-16 ${inter.className}`}
+      >
+        <header className='mb-32 flex flex-col items-start'>
+          <a
+            className='text-medium inline-block font-medium no-underline'
+            href='/'
+          >
+            Mike de Geofroy
+          </a>
+          <span className='text-medium font-medium leading-none text-muted'>
+            Software Engineer
+          </span>
         </header>
-        <main className='flex-grow flex flex-col'>
-          {children}
-        </main>
+        <main className='flex-grow flex flex-col'>{children}</main>
       </body>
     </html>
   );
