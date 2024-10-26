9ea24ae50951535c18dd0cd8867dc1e1166a9ee9
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-10-27 01:33:49 +0300

    updated webiste style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,12 @@
 import type { Metadata } from 'next';
-import Image from 'next/image';
-import arrowIcon from '@/public/arrow.svg';
-import localFont from 'next/font/local';
 import './globals.css';
-import Link from 'next/link';
-import Quote from '@/components/Quote';
 
-const featureMono = localFont({
-  src: [
-    {
-      path: './fonts/FeatureMono-Hairline.ttf',
-      weight: '100',
-      style: 'normal',
-    },
-    {
-      path: './fonts/FeatureMono-Thin.ttf',
-      weight: '200',
-      style: 'normal',
-    },
-    {
-      path: './fonts/FeatureMono-Light.ttf',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: './fonts/FeatureMono-Regular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './fonts/FeatureMono-Medium.ttf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: './fonts/FeatureMono-Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
-});
+import { Inter } from 'next/font/google'
+ 
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'mikedegeofroy.com',
@@ -67,25 +34,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`min-h-svh flex flex-col ${featureMono.className}`}>
-        <header className='flex w-screen overflow-x-hidden flex-col justify-between p-5 text-xl md:text-3xl font-light'>
-          <div className='flex justify-between'>
-            <Link href='/'>
-              <div className='flex items-center'>
-                <h1>SELECTED PROJECTS</h1>
-                <Image
-                  className='dark:invert h-4 w-4 md:h-5 md:w-5 m-1'
-                  priority
-                  src={arrowIcon}
-                  alt={'arrow icon'}
-                />
-              </div>
-            </Link>
-            <Quote />
-            <h1 className='hidden md:block'>{new Date().getFullYear()}</h1>
-          </div>
+      <body className={`mx-auto max-w-[692px] overflow-x-hidden px-6 py-12 text-gray-1200 antialiased sm:py-32 md:overflow-x-visible md:py-16 ${inter.className}`}>
+        <header className="mb-32 flex flex-col items-start">
+            <a className="text-medium inline-block font-medium no-underline" href="/">Mike de Geofroy</a>
+            <span className="text-medium font-medium leading-none text-muted">Software Engineer</span>
         </header>
-        <main className='flex-grow flex flex-col text-xl md:text-3xl font-light px-5 pb-5'>
+        <main className='flex-grow flex flex-col'>
           {children}
         </main>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,61 @@
-import Image from 'next/image';
-import arrowIcon from '@/public/arrow.svg';
 import Link from 'next/link';
 import { allDocs } from '@/.contentlayer/generated';
 
 const Home = () => {
-  return (
-    <div className='flex flex-grow overflow-x-hidden flex-col justify-between'>
-      <span></span>
-      <div className='flex flex-col gap-2'>
-        {allDocs
-          .filter((x) => x.selected)
-          .map((x, indx) => {
-            return (
-              <Link key={indx} href={x.slug}>
-                <h1 className='cursor-pointer w-fit'>{x.title}</h1>
-              </Link>
-            );
-          })}
-      </div>
-      <div className='flex-col'>
-        <div className='flex justify-between'>
-          <Link href={'/about'}>
-            <div className='flex items-center'>
-              <h1>DE GEOFROY</h1>
-              <Image
-                className='dark:invert h-4 w-4 md:h-5 md:w-5 m-1'
-                priority
-                src={arrowIcon}
-                alt={'arrow icon'}
-              />
-            </div>
-          </Link>
-          <Link href={'/about'}>
-            <h1 className='hidden md:block'>PROJECTS</h1>
-          </Link>
+  const blog = allDocs.filter((x) => x.selected && x.slug.match('blog'));
+  const projects = allDocs.filter(
+    (x) => x.selected && x.slug.match('projects')
+  );
 
-          <Link href={'/blog'}>
-            <h1>BLOG</h1>
-          </Link>
+  return (
+    <>
+      <span className='mb-5 font-medium'>Today</span>
+      <p className='text-muted'>
+        Software Engineering student at ITMO University and founder of
+        shamps.dev, passionate about building seamless digital experiences that
+        feel like magic.
+      </p>
+      {projects.length > 0 && (
+        <div className='mt-16 sm:mt-32'>
+          <span className='mb-5 block font-medium'>Projects</span>
+          <div className='flex flex-col gap-2'>
+            {projects.map((x, indx) => {
+              return (
+                <Link key={indx} href={x.slug}>
+                  <div className='-mx-3 flex flex-col rounded-md px-3 no-underline hover:bg-[#191918] sm:py-3'>
+                    <h1 className='cursor-pointer w-fit'>{x.title}</h1>
+                    <p className='text-muted'>{x.description}</p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
         </div>
-        <div className='h-[8rem] md:h-[12rem] flex items-center'>
-          <span className='text-[8rem] md:text-[12rem] font-medium'>mike*</span>
+      )}
+      {blog.length > 0 && (
+        <div className='mt-16 sm:mt-32'>
+          <span className='mb-5 block font-medium'>Blog</span>
+          <div className='flex flex-col gap-2'>
+            {blog.map((x, indx) => {
+              return (
+                <Link key={indx} href={x.slug}>
+                  <div className='-mx-3 flex flex-col rounded-md px-3 no-underline hover:bg-[#191918] sm:py-3'>
+                    <h1 className='cursor-pointer w-fit'>{x.title}</h1>
+                    <p className='text-muted'>{x.description}</p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
         </div>
+      )}
+      <div className='mt-16 sm:mt-36'>
+        <span className='mb-5 block font-medium'>More</span>
+        <p className='text-muted'>
+          You can contact me at <b>@mikedegeofroy</b> on telegram.
+        </p>
       </div>
-    </div>
+    </>
   );
 };
 
