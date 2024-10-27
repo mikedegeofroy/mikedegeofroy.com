1ed13be39fdfdab353666ddffec58b31efb2e328
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-10-27 03:55:20 +0300

    Develop (#4)
    * updated webiste style
    
    * added google analytics, and og scripts.. ( with twitter meta tags too )
    
    * updated the og-image
    
    * updated gaps on front page
    
    * Fixed light theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,9 +91,7 @@ export default function RootLayout({
             Software Engineer
           </span>
         </header>
-        <main className='flex-grow flex flex-col text-xl md:text-3xl font-light px-5 pb-5'>
-          {children}
-        </main>
+        <main className='flex-grow flex flex-col'>{children}</main>
       </body>
     </html>
   );
